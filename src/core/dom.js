dff62df25fbe42ca73c6f274dda43f2528112f41
Vladislav Zaporozhets <piman131@yandex.ua>
2021-03-30 00:51:42 +0300

    add resize table

--- src/core/dom.js
+++ src/core/dom.js
@@ -37,6 +37,28 @@ class Dom {
 	off(eventType, callback) {
 		this.$el.removeEventListener(eventType, callback)
 	}
+
+	get data() {
+		return this.$el.dataset
+	}
+
+	closest(selector) {
+		return $(this.$el.closest(selector))
+	}
+
+	getCords() {
+		return this.$el.getBoundingClientRect()
+	}
+
+	findAll(selector) {
+		return this.$el.querySelectorAll(selector)
+	}
+
+	css(styles = {}) {
+		Object
+			.keys(styles)
+			.forEach(key => this.$el.style[key] = styles[key])
+	}
 }
 
 export function $(selector) {
